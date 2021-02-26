739440bdad1e859a21b811cc4b85fc91173f5a6c
YikiSu <70603397+YikiSu@users.noreply.github.com>
2021-02-26 13:54:19 -0800

    Change the parameter name for input text
    Change the parameter name for input text from data to text to make it more clear

--- pyemoodji/pyemoodji.py
+++ pyemoodji/pyemoodji.py
@@ -1,13 +1,13 @@
 # Python script for pyemoodji package
 
-def sentiment_df(data, sentiment="all"):
+def sentiment_df(text, sentiment="all"):
     """
     Generates a sentiment analysis summary dataframe of the input text. The summary dataframe would include 
     the sentiment type, sentiment words, percentage of sentiments and highest sentiment percentage.
 
     Parameters:
     -----------
-        data (str): the input text for sentiment analysis
+        text (str): the input text for sentiment analysis
         sentiment (str, optional): the sentiment that the analysis focuses on, could be happy, positive or sad etc. Defaults to "all".
 
     Returns:
@@ -18,13 +18,13 @@ def sentiment_df(data, sentiment="all"):
     return result_df
 
 
-def sentiment_plot(data, sentiment = "happy", n=10, width=10, height=10):
+def sentiment_plot(text, sentiment = "happy", n=10, width=10, height=10):
     """
     Generates a plot to show the top n sentiment words in the input text file.
 
     Parameters:
     -----------
-        data (str): the input text for sentiment analysis
+        text (str): the input text for sentiment analysis
         sentiment (str, optional): the sentiment that the analysis focuses on. Defaults to "happy".
         n (int, optional): the number of sentiment words to show in the plot
         width (int, optional): the width of the output plot. Defaults to 10.
