7a5c40a2f355e7221f7b4fdaeb363282cc0d1f7d
Yiki Su <sueyi@student.ubc.ca>
2021-02-25 22:45:22 -0800

    finish docstrings for sentiment_df and sentiment_plot functions

---
+++ pyemoodji/pyemoodji.py
@@ -0,0 +1,36 @@
+# Python script for pyemoodji package
+
+def sentiment_df(data, sentiment="all"):
+    """
+    Generates a sentiment analysis summary dataframe of the input text. The summary dataframe would include 
+    the sentiment type, sentiment words, percentage of sentiments and highest sentiment percentage.
+
+    Parameters:
+    -----------
+        data (str): the input text for sentiment analysis
+        sentiment (str, optional): the sentiment that the analysis focuses on, could be happy, positive or sad etc. Defaults to "all".
+
+    Returns:
+    --------
+        data frame: a data frame that contains the summary of sentiment analysis
+    """
+
+    return result_df
+
+
+def sentiment_plot(data, sentiment = "happy", n=10, width=10, height=10):
+    """
+    Generates a plot to show the top n sentiment words in the input text file.
+
+    Parameters:
+    -----------
+        data (str): the input text for sentiment analysis
+        sentiment (str, optional): the sentiment that the analysis focuses on. Defaults to "happy".
+        n (int, optional): the number of sentiment words to show in the plot
+        width (int, optional): the width of the output plot. Defaults to 10.
+        height (int, optional): the height of the output plot. Defaults to 10.
+    
+    Returns:
+    --------
+        graph: a plot that shows the top n sentiment words of the input text file
+    """
