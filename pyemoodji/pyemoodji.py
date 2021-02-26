c3f2d8f6d8e5c5c0fd0fd6ac94aba69e373aab02
kshahnazari1998 <kshahnazari@gmail.com>
2021-02-25 21:45:24 -0800

    Text sentiment to emojo documentation written

---
+++ pyemoodji/pyemoodji.py
@@ -0,0 +1,31 @@
+def textsentiment_to_emoji(text):
+    """
+    Detect the word sentiments of a text and replace the
+    with the matching emojis.
+
+    Args:
+        text (String): A text string containing english words
+
+    Returns:
+        [String]: A string containing only emoji's with no words.
+            The emojis are written in the CLDR short name format.
+
+    example:
+
+        textsentiment_to_emoji("I am very happy")
+        returns: "\N{smiling face with smiling eyes}"
+    """
+    return None
+
+
+def text_counter(text):
+    """
+    Counts the number of characters, words and sentences of a string
+
+    Args:
+        text (String):
+
+    Returns:
+        [type]: [description]
+    """
+    return None
