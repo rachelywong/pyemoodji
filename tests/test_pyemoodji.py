1cf161ef5f7166f2d0e336eb7c8b9ee0273bb35a
Rachel Wong <rachelwong.main@gmail.com>
2021-03-04 15:45:26 -0800

    updated tests

--- tests/test_pyemoodji.py
+++ tests/test_pyemoodji.py
@@ -11,14 +11,14 @@ def test_sentiment_df(text = "I am happy"):
     assert isinstance(pyemoodji.sentiment_df(text), pd.DataFrame), 'output should be a dataframe'
     assert len(pyemoodji.sentiment_df(text).columns) == 5, 'output should have 5 columns'
     assert (pyemoodji.sentiment_df(text)).loc[0, "word_count"] == 1, 'output should be 1 (1 happy in text)'   
-    assert (pyemoodji.sentiment_df(text)).loc[0, "emotion_count"] == 1, 'output should be 1 (1 Happy in text)'  
+    assert (pyemoodji.sentiment_df(text)).set_index("word").loc["happy","emotion_count"] == 1
 
 # Tests for sentiment analysis plot function
 def test_sentiment_plot(text = "I am happy"):
     plot = pyemoodji.sentiment_plot(text)
-    assert plot.encoding.x.field == 'word', 'x_axis should be mapped to the x axis'
-    assert plot.encoding.y.field == 'word_count', 'y_axis should be mapped to the y axis'
-    assert plot.encoding.color.field == 'key', 'key should be mapped to the key'
+    assert plot.encoding.x.shorthand == 'word', 'x_axis should be mapped to the x axis'
+    assert plot.encoding.y.shorthand == 'word_count', 'y_axis should be mapped to the y axis'
+    assert plot.encoding.color.shorthand == 'key', 'key should be mapped to the key'
     assert plot.mark == 'bar', 'mark should be a bar'
     
 def test_textsentiment_to_emoji():
