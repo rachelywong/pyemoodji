6500f2e14ce7e7b879a919f5b866b106a26491cf
Rachel Wong <rachelwong.main@gmail.com>
2021-03-03 21:24:30 -0800

    finished tests with Yiki

--- tests/test_pyemoodji.py
+++ tests/test_pyemoodji.py
@@ -1,21 +1,21 @@
 from pyemoodji import __version__
 from pyemoodji import pyemoodji
+import pandas as pd
 
 def test_version():
     assert __version__ == '0.1.0'
 
 # Tests for sentiment analysis df function
-def test_sentiment_df():
-    assert small_scatter.encoding.x.field == 'year', 'x_axis should be mapped to the x axis'
-    assert small_scatter.encoding.y.field == 'measure', 'y_axis should be mapped to the y axis'
-    assert small_scatter.mark == 'line', 'mark should be a line'
-    assert small_scatter.encoding.x.scale.zero == False, "x-axis should not start at 0"
-    assert small_scatter.encoding.x.axis.tickMinStep == 1, "x-axis small tick step should be 1"
+def test_sentiment_df(text = "I am happy"):
+    assert isinstance(pyemoodji.sentiment_df(text), pd.DataFrame), 'output should be a dataframe'
+    assert len(pyemoodji.sentiment_df(text).columns) == 5, 'output should have 5 columns'
+    assert (pyemoodji.sentiment_df(text)).loc[0, "word_count"] == 1, 'output should be 1 (1 happy in text)'   
+    assert (pyemoodji.sentiment_df(text)).loc[0, "emotion_count"] == 1, 'output should be 1 (1 Happy in text)'  
 
 # Tests for sentiment analysis plot function
-def test_sentiment_plot():
-    assert small_scatter.encoding.x.field == 'year', 'x_axis should be mapped to the x axis'
-    assert small_scatter.encoding.y.field == 'measure', 'y_axis should be mapped to the y axis'
-    assert small_scatter.mark == 'line', 'mark should be a line'
-    assert small_scatter.encoding.x.scale.zero == False, "x-axis should not start at 0"
-    assert small_scatter.encoding.x.axis.tickMinStep == 1, "x-axis small tick step should be 1"
+def test_sentiment_plot(text = "I am happy"):
+    plot = pyemoodji.sentiment_plot(text)
+    assert plot.encoding.x.field == 'word', 'x_axis should be mapped to the x axis'
+    assert plot.encoding.y.field == 'word_count', 'y_axis should be mapped to the y axis'
+    assert plot.encoding.color.field == 'key', 'key should be mapped to the key'
+    assert plot.mark == 'bar', 'mark should be a bar'
